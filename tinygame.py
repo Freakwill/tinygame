2901497d40f341581c033e6c5736a107586445ec
William Song <30965609+Freakwill@users.noreply.github.com>
2018-02-23 19:45:43 +0800

    Update tinygame.py
    we use apscheduler from now on

--- tinygame.py
+++ tinygame.py
@@ -1,8 +1,39 @@
 # -*- coding: utf-8 -*-
-# Designer：Haojie Ma 马豪杰
 
 import random
-import time
+import collections
+
+from pytz import utc
+
+from pymongo import MongoClient
+from apscheduler.schedulers.blocking import *
+from apscheduler.jobstores.mongodb import MongoDBJobStore
+from apscheduler.jobstores.memory import MemoryJobStore
+from apscheduler.executors.pool import ThreadPoolExecutor, ProcessPoolExecutor
+from apscheduler.events import *
+
+import threading
+
+lock = threading.Lock()
+
+
+host = '127.0.0.1'
+port = 27017
+client = MongoClient(host, port)
+
+jobstores = {
+    'mongo': MongoDBJobStore(collection='job', database='game', client=client),
+    'default': MemoryJobStore()
+}
+executors = {
+    'default': ThreadPoolExecutor(10),
+    'processpool': ProcessPoolExecutor(3)
+}
+job_defaults = {
+    'coalesce': False,
+    'max_instances': 3
+}
+
 
 class Unit:
     def __init__(self,name):
@@ -10,6 +41,12 @@ class Unit:
 
     def __str__(self):
         return self.name
+
+    def after_dead(self):
+        pass
+
+    def when_init(self):
+        pass
 
 
 class Hero(Unit):
@@ -19,20 +56,37 @@ class Hero(Unit):
     damage: damage per second
     shield: the life of shield
     '''
-    def __init__(self, name, life, damage, shield):
+    def __init__(self, name, life, damage, shield, speed):
         self.name = name
         self.life0 = self.life = life
         self.damage = damage
         self.shield0 = self.shield = shield
+        self.speed = speed
+        self._army = None
 
     @staticmethod
     def fromStr(s):
         # str -> Hero
-        name, life, damage, shield = s.split()
-        return Hero(name, int(life), int(damage), int(shield))
+        name, life, damage, shield, speed = s.split()
+        return Hero(name, int(life), int(damage), int(shield), float(speed))
     
     def hit(self, other):
-        other.life -= self.damage
+        assert isinstance(other, Army)
+        with lock:
+            if other:
+                hero = other.random()
+            else:
+                raise ValueError('there is no hero left in the army %s'%other)
+            print('%s 攻击 %s'%(self, hero))
+            if not hero.shieldbreak():
+                hero.shield -= self.damage
+                if hero.shieldbreak():
+                    print('%s 护盾破裂'%hero)
+            else:
+                hero.life -= self.damage
+                if hero.isdead():
+                    other -= hero
+            return hero, other
 
     def shieldbreak(self):
         return self.shield <= 0
@@ -42,9 +96,8 @@ class Hero(Unit):
     
     def reset(self):
         self.life = self.life0
-        self.damage = self.damage0
-        
-import collections
+        self.shield = self.shield0
+
 
 class Army(Unit, collections.UserList):
     def __init__(self, name, heros):
@@ -55,12 +108,14 @@ class Army(Unit, collections.UserList):
         import copy
         return copy.deepcopy(self)
 
+    def random(self):
+        return random.choice(self)
+
     @staticmethod
     def fromStr(name, s):
         # str -> Army
         heros = s.split(';')
         return Army(name, [Hero.fromStr(h) for h in heros])
-
 
     def __iadd__(self, other):
         if isinstance(other, Hero):
@@ -87,88 +142,76 @@ class Army(Unit, collections.UserList):
         cpy -= other
         return cpy
 
-    # def shield(self):
-    #     return sum(hero.shield for hero in self.heros)
-
     @property
     def damage(self):
         return sum(hero.damage for hero in self)
  
-    def hit(self, other):
-        # 军队之间进攻核心程序
-        # 随机锁定敌方英雄攻击
-        hero = random.choice(other)
-        print('%s 受到攻击'%hero)
-        if not hero.shieldbreak():
-            # 护盾在，则由护盾抵抗伤害
-            hero.shield -= self.damage
-            if hero.shieldbreak():
-                # 护盾破裂，将不能再使用，但英雄不会死亡
-                print('%s 护盾破裂'%hero)
-        else:
-            # 护盾不在，由英雄生命抵抗伤害
-            hero.life -= self.damage
-            if hero.isdead():
-                # 英雄死亡
-                other -= hero
-                print('%s 阵亡'%hero)
-            else:
-                print('%s 还剩余生命值 %d'%(hero, hero.life))
-
     def isdead(self):
         return len(self) == 0
 
 
-_defaultWeapons = ('弓箭', '剑', '刀', '矛', '弩')
+
 class Game(object):
     '''Game has 3 (principal) propteries
     title: title
     army1: army1
     army2: army2'''
-    def __init__(self, title, army1, army2):
+    def __init__(self, title, army1=None, army2=None):
         self.title = title
         self.army1 = army1
         self.army2 = army2
-        self.bgm = ''
-        self.weapons = _defaultWeapons
+        self._scheduler = None
+
+    def init(self):
+        # override the method to create a new game
+        army1 = Army.fromStr('袁军', '袁绍 500 10 100 1;颜良 200 30 30 1;文丑 200 30 30 2')
+        army2 = Army.fromStr('曹军', '曹操 800 30 200 2;荀彧 100 20 600 3;许褚 100 20 10 1')
+
+        scheduler = BlockingScheduler(jobstores=jobstores, executors=executors, job_defaults=job_defaults, timezone=utc)
+
+        for h in army1:
+            scheduler.add_job(h.hit, 'interval', args=(army2,), seconds=h.speed, id=str(h))
+        for h in army2:
+            scheduler.add_job(h.hit, 'interval', args=(army1,), seconds=h.speed, id=str(h))
+
+        # def check(army1, army2):
+        #     if army1.isdead() or army2.isdead():
+        #         scheduler.shutdown()
+        # scheduler.add_job(check, 'interval', args=(army1, army2), seconds=5, id='check')
+
+        def rmjob(evt):
+            # remove a job
+            hero, army = evt.retval
+            if hero:
+                if hero.isdead():
+                    scheduler.remove_job(str(hero))
+                    print('%s 阵亡'%hero)
+                else:
+                    print('%s 还剩余生命值 %d'%(hero, hero.life))
+                if army.isdead():
+                    try:
+                        scheduler.shutdown()
+                    except RuntimeError:
+                        print('%s 全军覆没'%army)
+
+        scheduler.add_listener(rmjob, EVENT_JOB_EXECUTED)
+        self._scheduler = scheduler
+
 
     def start(self):
-        weapons = self.weapons
-        army1, army2 = self.army1, self.army2
-        print(self.title)
-        print(self.bgm)
-        k = 0
-        while True:
-            time.sleep(1)
-            k += 1
-            print('第 %d 回合:'%k)
-            if random.random() < 1:
-                weapon = random.choice(weapons)
-                print('%s 用 %s 攻击 %s'%(army1, weapon, army2))
-                army1.hit(army2)
-
-            if random.random() < .5:
-                weapon = random.choice(weapons)
-                print('%s 用 %s 攻击 %s'%(army2, weapon, army1))
-                army2.hit(army1)
-
-            if army1.isdead():
-                print('%s 全军覆没'%army1)
-                print('%s 剩下 %d 个英雄'%(army2, len(army2)))
-                break
-            if army2.isdead():
-                print('%s 全军覆没'%army2)
-                print('%s 剩下 %d 个英雄 '%(army1 , len(army1)))
-                break
+        try:
+            self._scheduler.start()
+        except ValueError as v:
+            print(v)
+        finally:
+            client.close()
 
 
 if __name__ =="__main__":
-    army1 = Army.fromStr('袁军', '袁绍 500 10 100;颜良 200 30 30;文丑 200 30 30')
-    army2 = Army.fromStr('曹军', '曹操 800 30 200;荀彧 100 20 600;许褚 100 20 10')
-    army2 += Hero.fromStr('关羽 200 20 20')
 
-    threeKingdoms = Game('官渡之战：东汉末年三大战役之一', army1, army2)
-    threeKingdoms.bgm ='BGM: 滚滚长江东逝水......'
+    threeKingdoms = Game('官渡之战：东汉末年三大战役之一')
+    threeKingdoms.init()
     threeKingdoms.start()
+    
 
 
